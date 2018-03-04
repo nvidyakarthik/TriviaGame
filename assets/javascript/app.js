b1805c7a6c1f6d06d0d2d4bcf83fad9b319451b5
nvidya.karthik <nvidya.karthik@gmail.com>
2018-03-04 13:50:15 -0700

    added comments to files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,8 +14,6 @@ var questionBank = [
         choices: ["Anaconda", "Reticulated python", "Green tree snake", "Corn snake"],
         answer: "Reticulated python"
     }
-
-
 ]
 var seconds = 0;
 var setIntervalId = 0;
@@ -26,17 +24,15 @@ var incorrect = 0;
 var correct = 0;
 var unanswered = 0;
 
+//Starts the quiz
 function startQuiz() {
-
     $("#startBtn").hide();
-    //$("#resultDiv").hide();
-    displayQuestion();
+     displayQuestion();
 }
 
+//This function restarts the quiz
 function restartQuiz() {
     $("#resultDiv").empty();
-    //  $("#mainDiv").hide();
-    //$("#resultDiv").hide();
     questionNum = 0;
     correct = 0;
     incorrect = 0;
@@ -44,70 +40,64 @@ function restartQuiz() {
     displayQuestion();
 }
 
-
+//This function sets the count down timer 30 to 0 secs for each question
 function countDown(questionId) {
-
-    //var timerCheck=0; 
-
-
+    //Checks if the seconds is not going negative
     if (seconds > 0) {
-       $("#displayTime").html("Time remaining "+seconds+" Seconds");
-       //$("#displayTime").html(seconds);
-        seconds--;
-        //timerCheck=setTimeout(countDown(seconds),1000);
+       $("#displayTime").html("Time remaining "+seconds+" Seconds");       
+        seconds--;        
     }
     else {
-
         clearInterval(setTimer);
         $("#displayTime").html("Time remaining "+seconds+" Seconds");
-       // $("#displayTime").html(seconds);
-       // $("#question").empty();
         $("#choices").empty();
         $("#dispAnswer").html("Out of Time!!");
         $("#dispAnswer").append("<p>The correct answer was " + questionBank[questionId].answer);
+        //tracking the unanswered questions here 
         unanswered++;
+        //This variable takes us to the next question set
         questionNum++;
-
+        //Displays the results for 3 secs and moves to the next question
         setIntervalId = setTimeout(function () {
             displayQuestion();
         }, 3000);
-
-
     }
-
-
 }
-$('#choices').on('click', '.choice', function(){
 
-    
+//This function checks the clicked answer
+$('#choices').on('click', '.choice', function(){    
     var answerId=$(this).attr("id");
     var questionId=$(this).attr("questNo");
     console.log(questionId);
-
- 
-/*function checkAnswer(answerId, questionId) { */
-
+    //This condition checks if the selected answer is correct or incorrect
     if (questionBank[questionId].choices[answerId] != questionBank[questionId].answer) {
+        //stoping the timer here to display the message
         clearInterval(setTimer);
         //$("#question").empty();
         $("#choices").empty();
         $("#dispAnswer").html("Your answer was incorrect the correct answer is: " + questionBank[questionId].answer);
+        //tracking the incorrect answers here
         incorrect++;
+        //This variable takes us to the next question set
         questionNum++;
+        //if the answer is incorrect displays the message to the user and moves on to the next question after 3 secs
         setIntervalId = setTimeout(function () {
             displayQuestion();
         }, 3000);
 
     }
     else {
+        //stoping the timer here to display the message
         clearInterval(setTimer);
         //$("#question").empty();
         $("#choices").empty();
         $("#dispAnswer").html("Congratulations!!!");
         $("#dispAnswer").append("<p>You answered correctly as " + questionBank[questionId].answer);
+        //tracking the correct answers here
         correct++;
+        //This variable take us to the next question set
         questionNum++;
-
+        //if the answer is correct display the message to the user and moves on to the next question after 3 secs
         setIntervalId = setTimeout(function () {
             displayQuestion();
         }, 3000);
@@ -115,13 +105,12 @@ $('#choices').on('click', '.choice', function(){
     }
 });
 
-
+//This function displays all the results
 function displayResults() {
+    //clearing the timer divs and display answer divs
     $("#dispAnswer").empty();
     $("#displayTime").empty();
     $("#question").html("Thankyou for taking the Quiz!!! Here is your results!!!");
-    
-    //$("#resultDiv").html('<p>All done,Here\'s how you did!!</p>');
     $("#resultDiv").html('<p>Correct Answers :<span id="correct">' + correct + '</span></p>');
     $("#resultDiv").append('<p>Incorrect Answers :<span id="incorrect">' + incorrect + '</span></p>');
     $("#resultDiv").append('<p>Unanswered :<span id="notanswered">' + unanswered + '</span></p>');
@@ -129,6 +118,7 @@ function displayResults() {
     $("#resultDiv").show();
 }
 
+//This function displays the question,sets the timer for the question and choices
 function displayQuestion() {
 
     if (questionNum == 3 || questionNum > 3) {
@@ -138,27 +128,28 @@ function displayQuestion() {
         displayResults();
     }
     else {
+        //clears the previous timer
         clearTimeout(setIntervalId);
 
         $("#choices").empty();
         $("#dispAnswer").empty();
         seconds = 10;
+        //sets timer for the question
         setTimer = setInterval(function () {
             countDown(questionNum);
         }, 1000); 
+        //displays the question
         $("#question").html((questionNum+1)+"."+questionBank[questionNum]["question"]);
+        //dynamically generating the choices buttons
         for (i = 0; i < questionBank[questionNum]["choices"].length; i++){
             var childDiv=$("<button>");
             childDiv.attr('id',i);
             childDiv.attr('questNo',questionNum);
             childDiv.attr('class','choice btn btn-primary col-xs-4 col-md-offset-2');
             childDiv.html(questionBank[questionNum].choices[i]);
-            $("#choices").append(childDiv);          
-            
-       
+            $("#choices").append(childDiv);           
         }
         console.log(questionBank[questionNum].question);
-
         console.log(questionNum);
     }
 
